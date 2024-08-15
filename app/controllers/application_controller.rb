dc54fcaeb6e12264cadfab11390618954dc2022c
rOman4ike <rOman4ike@yandex.ru>
2024-08-16 01:34:08 +0700

    create user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user
+  include ActionController::Cookies
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
 end
